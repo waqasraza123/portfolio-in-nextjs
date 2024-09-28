8872900534642c0f4d813fd69b8eb786506d8deb
Waqas Raza <waqasraza123@gmail.com>
2024-09-28 21:28:33 +0500

    adjust hero and video section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,12 +2,12 @@
 
 export default function Hero() {
   return (
-    <section className="relative flex flex-col justify-center items-center h-screen bg-white text-gray-800 overflow-hidden">
+    <section className="relative flex flex-col justify-center items-center mt-8 bg-white text-gray-800 overflow-hidden">
       {/* Background animation */}
-      <div className="absolute inset-0 z-0 bg-gradient-to-r from-gray-100 to-gray-200 animate-gradient-xy"></div>
+      <div className="absolute inset-0 z-0 bg-gradient-to-r from-gray-100 to-gray-200"></div>
 
       {/* Hero Content */}
-      <div className="relative z-10 text-center">
+      <div className="relative z-10 text-center py-8 pt-16">
         {/* Introductory Text */}
         <h1 className="text-6xl md:text-8xl font-extrabold mb-4 tracking-tight animate-fade-in">
           I&apos;M <span className="text-blue-500">WAQAS</span>
@@ -24,7 +24,7 @@ export default function Hero() {
           <img
             src="/images/attention-to-details.jpg"
             alt="Eye"
-            className="w-40 h-20 object-cover rounded-full shadow-lg inline-block ml-4 animate-fade-in delay-300"
+            className="w-32 h-16 object-cover rounded-full shadow-lg inline-block ml-4 animate-fade-in delay-300"
           />
           <span className="text-blue-500"> WEB</span>
         </h2>
@@ -35,9 +35,9 @@ export default function Hero() {
 
         {/* Call-to-Action */}
         <div className="mt-10 animate-fade-in delay-700">
-          <button className="px-8 py-3 font-semibold text-white bg-blue-600 rounded-lg shadow-lg hover:bg-blue-700 transition ease-in-out duration-300">
-            Discover More
-          </button>
+          <a target="_blank" href="https://www.upwork.com/freelancers/waqasraza" className="px-8 py-3 font-semibold text-white bg-blue-600 rounded-lg shadow-lg hover:bg-blue-700 transition ease-in-out duration-300">
+            UpWork
+          </a>
         </div>
       </div>
     </section>
