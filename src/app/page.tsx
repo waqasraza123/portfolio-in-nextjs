421ec3ce8d121480ae44d8ff4c8dc4a81dcfb52d
Waqas Raza <waqasraza123@gmail.com>
2025-12-27 12:18:07 +0500

    Remove Projects feed section and project images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Head from 'next/head';
 import Header from '../components/Header';
 import About from '../components/About';
 import Testimonials from '../components/Testimonials';
-import Projects from '../components/Projects';
 import Hero from '../components/HeroSection';
 
 export default function Home() {
@@ -14,7 +13,6 @@ export default function Home() {
             <Header />
             <Hero />
             <main>
-                <Projects />
                 <Testimonials />
                 <About />
             </main>
