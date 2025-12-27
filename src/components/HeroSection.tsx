9198b6fb78313ee7cc919dd7a3a615fef3e3223b
Waqas Raza <waqasraza123@gmail.com>
2025-12-27 12:20:38 +0500

    Remove inline images from hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -10,22 +10,11 @@ export default function Hero() {
       <div className="relative z-10 text-center py-8 pt-16">
         {/* Introductory Text */}
         <h1 className="text-6xl md:text-8xl font-extrabold mb-4 tracking-tight animate-fade-in">
-          I&apos;M <span className="text-blue-500">WAQAS</span>
-          <img
-            src="/images/waqas-raza-upwork.jpg"
-            alt="Profile"
-            className="w-20 h-20 object-cover rounded-full border-4 border-white shadow-lg inline-block ml-4 animate-fade-in"
-          />
-          <span className="text-blue-500"> R.</span>
+          I&apos;M <span className="text-blue-500">WAQAS R.</span>
         </h1>
 
         <h2 className="text-5xl md:text-7xl font-extrabold mb-4 animate-fade-in">
           <span className="text-black">FULL STACK</span>
-          <img
-            src="/images/attention-to-details.jpg"
-            alt="Eye"
-            className="w-32 h-16 object-cover rounded-full shadow-lg inline-block ml-4 animate-fade-in delay-300"
-          />
           <span className="text-blue-500"> WEB</span>
         </h2>
 
